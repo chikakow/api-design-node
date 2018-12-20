76acbd348bab6a31332cd829ea74b00e582dd04b
Chikako Wakishima <ChikakoW@sedata.com>
2018-12-20 16:11:10 -0500

    my work done

--- server/spec.js
+++ server/spec.js
@@ -6,8 +6,7 @@ var expect = require('chai').expect;
 // DELETE, UPDATE, PUT, GET ONE
 // to run the test type mocha server/specs.js
 
-describe('[LIONS]', function(){
-
+describe('[LIONS]', function() {
   it('should get all lions', function(done) {
     request(app)
       .get('/lions')
@@ -17,8 +16,62 @@ describe('[LIONS]', function(){
       .end(function(err, resp) {
         expect(resp.body).to.be.an('array');
         done();
-      })
+      });
+  });
+
+  it('should post a lion', done => {
+    const lion = { name: 'chikako', age: 42, character: 'calm' };
+
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, resp) => {
+        expect(resp.body).to.be.an('object');
+        expect(resp.body.name).to.eqls('chikako');
+        // if you use eqls it's comparing properties deep equal
+        // if you use to.equals then it has to be the same object. toBe for jasmin
+        // expect(resp.body).to.eqls(lion);
+        done();
+      });
+  });
+
+  it('should update lion', done => {
+    const lion = { name: 'chikako', age: 42, character: 'calm' };
+
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .end((err, resp) => {
+        const updateLion = resp.body;
+        const newLion = { ...updateLion, name: 'chi', age: 29 };
+        request(app)
+          .put('/lions/' + updateLion.id)
+          .send(newLion)
+          .end((err, resp) => {
+            expect(resp.body).to.eql(newLion);
+            done();
+          });
+      });
+  });
+
+  it('should delete lion', done => {
+    const lion = { name: 'chikako', age: 42, character: 'calm' };
+    request(app)
+      .post('/lions')
+      .send(lion)
+      .set('Accept', 'application/json')
+      .end((err, resp) => {
+        const createdLion = resp.body;
+        request(app)
+          .delete(`/lions/${createdLion.id}`)
+          .end((err, resp) => {
+            expect(resp.body).to.eqls(createdLion);
+            done();
+          });
+      });
   });
 });
-
-

43f8e1488de728556a839aefc1e1b9da82cf634a
Chikako Wakishima <ChikakoW@sedata.com>
2018-12-27 14:52:47 -0500

    hooking up to mondo local db and working!! inserting getting

--- server/api/user/userRoutes.js
+++ server/api/user/userRoutes.js
@@ -1,12 +1,19 @@
 var router = require('express').Router();
 var logger = require('../../util/logger');
 
+var controller = require('./userController');
+
 // setup boilerplate route jsut to satisfy a request
 // for building
-router.route('/')
-  .get(function(req, res){
-    logger.log('Hey from user!!');
-    res.send({ok: true});
-  });
+router.param('id', controller.params);
 
+router.route('/').get(controller.get);
+
+router.route('/:id').get(controller.getOne);
+
+router.route('/:id').put(controller.put);
+
+router.route('/').post(controller.post);
+
+router.route('/:id').delete(controller.delete);
 module.exports = router;
